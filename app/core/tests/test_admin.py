62fc50d22275157f9f70191c5f4c00d84a0e3768
mallardknundrum <mallardknundrum@gmail.com>
2023-04-09 18:44:50 -0700

    small updates

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,5 +1,6 @@
 """
-Tests for the Django admin modifications."""
+Tests for the Django admin modifications.
+"""
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -14,7 +15,7 @@ class AdminSiteTests(TestCase):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
             email='admin@example.com',
-            password = 'testpass123'
+            password='testpass123'
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
